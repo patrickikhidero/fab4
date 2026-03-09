5e7e68497b9eb8381fb305437cf23ee968d31518
WebCrawler <olatidejoseph17@gmail.com>
2026-03-09 23:04:36 +0100

    Disable additional ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,7 +21,9 @@ const eslintConfig = [
     ],
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-empty-object-type": "off"
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "react/no-unescaped-entities": "off"
   }
   },
 ];
